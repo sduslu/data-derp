0778403810217103a217e691f31b3ac2271c45f3
Kelsey Mok <kelseymok@gmail.com>
2021-04-17 16:45:09 +0200

    km: separating pipeline jobs for different scripts and using the right entrypoint script

--- data-ingestion/main.tf
+++ data-ingestion/main.tf
@@ -4,7 +4,7 @@ module "glue-job" {
   project-name = var.project-name
   module-name = var.module-name
   submodule-name = "data-ingestion"
-  script-path = "ingestion.py"
+  script-path = "main.py"
   additional-params = {
     "--extra-py-files": "s3://${var.project-name}-${var.module-name}/ingestion_config.zip,s3://${var.project-name}-${var.module-name}/ingestion.zip"
     }
