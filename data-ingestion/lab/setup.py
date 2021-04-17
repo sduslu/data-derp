6f6503e5e092397111fc242bc16a89b310cb0045
Kelsey Mok <kelseymok@gmail.com>
2021-04-17 19:43:14 +0200

    km: reverting to packages

--- data-ingestion/lab/setup.py
+++ data-ingestion/lab/setup.py
@@ -3,6 +3,5 @@ setup(
     name="data_ingestion",
     version="0.1",
     # packages=find_packages()
-    # packages=["data_ingestion"]
-    py_modules = ['ingestion','parameters']
+    packages=["data_ingestion"]
 )
