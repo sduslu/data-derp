dd947365e0054e67b26b3f8146361951b49d64fd
Kelsey Mok <kelseymok@gmail.com>
2021-04-17 19:43:29 +0200

    km: adding documentation

--- data-ingestion/lab/setup.py
+++ data-ingestion/lab/setup.py
@@ -3,5 +3,5 @@ setup(
     name="data_ingestion",
     version="0.1",
     # packages=find_packages()
-    packages=["data_ingestion"]
+    packages=["data_ingestion"] #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
 )
