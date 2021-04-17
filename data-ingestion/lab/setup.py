47254df5040aa2d143d13778de64b78066e3bb89
Kelsey Mok <kelseymok@gmail.com>
2021-04-17 19:36:34 +0200

    km: using py_modules and adding an init.py

--- data-ingestion/lab/setup.py
+++ data-ingestion/lab/setup.py
@@ -2,5 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="data_ingestion",
     version="0.1",
-    packages=find_packages()
+    # packages=find_packages()
+    # packages=["data_ingestion"]
+    py_modules = ['ingestion','parameters']
 )
