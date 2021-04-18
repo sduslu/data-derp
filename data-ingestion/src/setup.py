3d101e31b2e661d0b9dae5ae4cc160debed236be
Kelsey Mok <kelseymok@gmail.com>
2021-04-18 12:33:33 +0200

    km: fixing stray lib

--- data-ingestion/src/setup.py
+++ data-ingestion/src/setup.py
@@ -2,6 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="data_ingestion",
     version="0.1",
-    # packages=find_packages()
-    packages=["lib.data_ingestion"] #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
+    packages=["data_ingestion"] #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
 )
