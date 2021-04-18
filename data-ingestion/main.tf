8c58f599edc71b0b6bb0e6d6e295f98e5dc59ea0
Kelsey Mok <kelseymok@gmail.com>
2021-04-18 13:04:36 +0200

    km: moving eggs and main.py into their submodule dirs, commenting out the additional-python-modules

--- data-ingestion/main.tf
+++ data-ingestion/main.tf
@@ -4,9 +4,9 @@ module "glue-job" {
   project-name = var.project-name
   module-name = var.module-name
   submodule-name = "data-ingestion"
-  script-path = "main.py"
+  script-path = "data-ingestion/main.py"
   additional-params = {
-    "--additional-python-modules": "s3://${var.project-name}-${var.module-name}/data_ingestion-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
-    "--extra-py-files": "s3://${var.project-name}-${var.module-name}/data_ingestion-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
+//    "--additional-python-modules": "s3://${var.project-name}-${var.module-name}/data-ingestion/data_ingestion-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
+    "--extra-py-files": "s3://${var.project-name}-${var.module-name}/data-ingestion/data_ingestion-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
   }
 }
--- data-transformation/main.tf
+++ data-transformation/main.tf
@@ -4,9 +4,9 @@ module "glue-job" {
   project-name = var.project-name
   module-name = var.module-name
   submodule-name = "data-transformation"
-  script-path = "main.py"
+  script-path = "data-transformation/main.py"
   additional-params = {
-    "--additional-python-modules": "s3://${var.project-name}-${var.module-name}/data_transformation-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
-    "--extra-py-files": "s3://${var.project-name}-${var.module-name}/data_transformation-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
+//    "--additional-python-modules": "s3://${var.project-name}-${var.module-name}/data-transformation/data_transformation-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
+    "--extra-py-files": "s3://${var.project-name}-${var.module-name}/data-transformation/data_transformation-0.1-py3.egg", # https://docs.aws.amazon.com/glue/latest/dg/reduced-start-times-spark-etl-jobs.html
   }
 }
