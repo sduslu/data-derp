88832d03dd97bcf7a5986af9c6b198acd46eae8a
Pawarit Laosunthara <39638268+PawaritL@users.noreply.github.com>
2021-04-22 23:59:19 +0200

    KM/PL move download function to top of file

--- data-ingestion/src/data_ingestion/config.py
+++ data-ingestion/src/data_ingestion/config.py
@@ -4,10 +4,27 @@ import pandas as pd
 
 # ---------- Part I: Job Setup ---------- #
 
+def download(path):
+    """Anonymously downloads csv from S3 to a custom (potentially non-existent) destination"""
+    if ("/" not in path) or (".csv" not in path):
+        raise ValueError("path should be of format ./.../<filename>.csv")
+
+    filename = path.split("/")[-1]
+    folder = path.replace(filename, "")
+
+    s3_path = "s3://twdu-germany-data-source/" + filename
+    pandas_df = pd.read_csv(s3_path) # NOTE: to read from S3 straight from pd.read_csv, make sure you've already pip installed s3fs"
+
+    # create destination folder(s) if doesn't exist so that Pandas can successfully write the csv into the folder
+    if not os.path.exists(folder): 
+        os.makedirs(folder)
+
+    pandas_df.to_csv(path, index=False) # download as csv to local storage (for local development only)
+    return
+
 # By sticking with standard Spark, we can avoid having to deal with Glue dependencies locally
 # If developing outside of the TWDU Dev Container, don't forget to set the environment variable: TWDU_ENVIRONMENT=local
 ENVIRONMENT = os.getenv(key="TWDU_ENVIRONMENT", default="aws")
-
 
 if ENVIRONMENT not in ["local", "aws"]:
     raise ValueError("""ENVIRONMENT must be "local" or "aws" only""")
@@ -47,21 +64,3 @@ elif ENVIRONMENT == "local":
         "co2_input_path":                   "/workspaces/twdu-germany/data-ingestion/tmp/input-data/EmissionsByCountry.csv",
         "co2_output_path":                  "/workspaces/twdu-germany/data-ingestion/tmp/output-data/EmissionsByCountry.parquet",
     }
-
-    def download(path):
-        """Anonymously downloads csv from S3 to a custom (potentially non-existent) destination"""
-        if ("/" not in path) or (".csv" not in path):
-            raise ValueError("path should be of format ./.../<filename>.csv")
-
-        filename = path.split("/")[-1]
-        folder = path.replace(filename, "")
-
-        s3_path = "s3://twdu-germany-data-source/" + filename
-        pandas_df = pd.read_csv(s3_path) # NOTE: to read from S3 straight from pd.read_csv, make sure you've already pip installed s3fs"
-
-        # create destination folder(s) if doesn't exist so that Pandas can successfully write the csv into the folder
-        if not os.path.exists(folder): 
-            os.makedirs(folder)
-
-        pandas_df.to_csv(path, index=False) # download as csv to local storage (for local development only)
-        return
