54e207cf55ec01666e02d7fedc0355d77ba16118
Pawarit Laosunthara <pawarit.laosunthara@thoughtworks.com>
2021-04-24 23:09:41 +0000

    PL make buckets easily accessible

--- terraform-modules/s3-bucket/s3.tf
+++ terraform-modules/s3-bucket/s3.tf
@@ -31,7 +31,6 @@ resource "aws_s3_bucket_policy" "this" {
         Principal = "*"
         Action    = "s3:GetObject"
         Resource = [
-          aws_s3_bucket.this.arn,
           "${aws_s3_bucket.this.arn}/*",
         ]
       },
