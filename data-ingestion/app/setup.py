4a59e1db6ac890672d5e331c5472509e33422dc3
Kelsey Mok <kelseymok@gmail.com>
2021-04-26 15:27:02 +0200

    km: finding packages programatically

--- data-ingestion/app/setup.py
+++ data-ingestion/app/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="data_ingestion",
     version="0.1",
-    packages=["data_ingestion"] #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
+    packages=find_packages(where='src') #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
 )
--- data-transformation/app/setup.py
+++ data-transformation/app/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="data_transformation",
     version="0.1",
-    packages=["data_transformation"] #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
+    packages=find_packages(where='src') #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
 )
