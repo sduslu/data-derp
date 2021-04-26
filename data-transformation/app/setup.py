d6a322dc7550c39636b2a4a5c25fbef90e2985c8
Kelsey Mok <kelseymok@gmail.com>
2021-04-26 16:13:36 +0200

    pl/km: adding package_dir src to data-ingestion and transformation

--- data-transformation/app/setup.py
+++ data-transformation/app/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name="data_transformation",
     version="0.1",
-    packages=find_packages(where='src') #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
+    packages=find_packages(where='src'), #https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html#create-python-extra-library
+    package_dir={'': 'src'}
 )
