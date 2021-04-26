17c9bb5cce848284804ab980b13366d56e49ab6a
Kelsey Mok <kelseymok@gmail.com>
2021-04-26 10:27:00 +0200

    km: commenting out line for file that doesn't exist - check later

--- bootstrap/create-update-source.sh
+++ bootstrap/create-update-source.sh
@@ -41,8 +41,8 @@ create-update-stack() {
 
 upload_data() {
   pushd "${script_dir}" > /dev/null
-
-    AWS_PROFILE=default aws s3 cp "TemperaturesByCountryOriginal.csv" "s3://twdu-europe-data-source/TemperaturesByCountryOriginal.csv"
+# This file doesn't exist??
+#    AWS_PROFILE=default aws s3 cp "TemperaturesByCountryOriginal.csv" "s3://twdu-europe-data-source/TemperaturesByCountryOriginal.csv"
     AWS_PROFILE=default aws s3 cp "TemperaturesByCountry.csv" "s3://twdu-europe-data-source/TemperaturesByCountry.csv"
     AWS_PROFILE=default aws s3 cp "GlobalTemperatures.csv" "s3://twdu-europe-data-source/GlobalTemperatures.csv"
     AWS_PROFILE=default aws s3 cp "EmissionsByCountry.csv" "s3://twdu-europe-data-source/EmissionsByCountry.csv"
