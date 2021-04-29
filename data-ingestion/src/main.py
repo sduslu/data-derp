19ca6184e95b3a49d9ef92415fe6eb4bdf125eb6
Pawarit Laosunthara <39638268+PawaritL@users.noreply.github.com>
2021-04-29 22:04:39 +0200

    Update main.py

--- data-ingestion/src/main.py
+++ data-ingestion/src/main.py
@@ -6,7 +6,7 @@ from data_ingestion.config import job_parameters
 from data_ingestion.ingestion import Ingester
 
 # By sticking with standard Spark, we can avoid having to deal with Glue dependencies locally
-# If developing outside of the TWDU Dev Container, don't forget to set the environment variable: TWDU_ENVIRONMENT=local
+# If developing outside of the twdu-dev-container, don't forget to set the environment variable: TWDU_ENVIRONMENT=local
 ENVIRONMENT = os.getenv(key="TWDU_ENVIRONMENT", default="aws")
 
 # ---------- Part III: Run Da Ting (for Part II, see data_ingestion/ingestion.py) ---------- #
